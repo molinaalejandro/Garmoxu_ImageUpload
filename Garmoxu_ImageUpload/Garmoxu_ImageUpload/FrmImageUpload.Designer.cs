48661042121a9f84a3eb0aa61e6a55f9aadee874
Alejandro Molina <80757262+molinaalejandro@users.noreply.github.com>
2022-05-23 20:48:06 +0200

    v1.0.0
    -Creación completa de la aplicación

---
+++ Garmoxu_ImageUpload/Garmoxu_ImageUpload/FrmImageUpload.Designer.cs
@@ -0,0 +1,279 @@
+﻿
+namespace Garmoxu_ImageUpload
+{
+    partial class FrmImageUpload
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmImageUpload));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.RdbLocal = new RJCodeAdvance.RJControls.RJRadioButton();
+            this.RdbRemoto = new RJCodeAdvance.RJControls.RJRadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.BtnPlatos = new RJCodeAdvance.RJControls.RJButton();
+            this.BtnCategorias = new RJCodeAdvance.RJControls.RJButton();
+            this.BtnUsuarios = new RJCodeAdvance.RJControls.RJButton();
+            this.BtnTodo = new RJCodeAdvance.RJControls.RJButton();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(54)))), ((int)(((byte)(58)))));
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 70F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 70F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.BtnTodo, 1, 7);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 9;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 70F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 70F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 70F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1027, 703);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.RdbLocal, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.RdbRemoto, 1, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(73, 174);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(881, 100);
+            this.tableLayoutPanel2.TabIndex = 7;
+            // 
+            // RdbLocal
+            // 
+            this.RdbLocal.AutoSize = true;
+            this.RdbLocal.CheckedColor = System.Drawing.Color.MediumSlateBlue;
+            this.RdbLocal.Font = new System.Drawing.Font("Source Sans Pro", 16.2F, System.Drawing.FontStyle.Bold);
+            this.RdbLocal.ForeColor = System.Drawing.Color.White;
+            this.RdbLocal.Location = new System.Drawing.Point(311, 3);
+            this.RdbLocal.MinimumSize = new System.Drawing.Size(0, 21);
+            this.RdbLocal.Name = "RdbLocal";
+            this.RdbLocal.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.RdbLocal.Size = new System.Drawing.Size(229, 40);
+            this.RdbLocal.TabIndex = 10;
+            this.RdbLocal.Text = "Conexión local";
+            this.RdbLocal.UnCheckedColor = System.Drawing.Color.Gray;
+            this.RdbLocal.UseVisualStyleBackColor = true;
+            this.RdbLocal.CheckedChanged += new System.EventHandler(this.RdbLocal_CheckedChanged);
+            // 
+            // RdbRemoto
+            // 
+            this.RdbRemoto.AutoSize = true;
+            this.RdbRemoto.CheckedColor = System.Drawing.Color.MediumSlateBlue;
+            this.RdbRemoto.Font = new System.Drawing.Font("Source Sans Pro", 16.2F, System.Drawing.FontStyle.Bold);
+            this.RdbRemoto.ForeColor = System.Drawing.Color.White;
+            this.RdbRemoto.Location = new System.Drawing.Point(311, 53);
+            this.RdbRemoto.MinimumSize = new System.Drawing.Size(0, 21);
+            this.RdbRemoto.Name = "RdbRemoto";
+            this.RdbRemoto.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.RdbRemoto.Size = new System.Drawing.Size(259, 40);
+            this.RdbRemoto.TabIndex = 9;
+            this.RdbRemoto.Text = "Conexión remota";
+            this.RdbRemoto.UnCheckedColor = System.Drawing.Color.Gray;
+            this.RdbRemoto.UseVisualStyleBackColor = true;
+            this.RdbRemoto.CheckedChanged += new System.EventHandler(this.RdbRemoto_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Source Sans Pro", 24.2F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(73, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(881, 51);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Image Upload";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 5;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel3.Controls.Add(this.BtnPlatos, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.BtnCategorias, 2, 0);
+            this.tableLayoutPanel3.Controls.Add(this.BtnUsuarios, 4, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(73, 350);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(881, 142);
+            this.tableLayoutPanel3.TabIndex = 9;
+            // 
+            // BtnPlatos
+            // 
+            this.BtnPlatos.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnPlatos.BackColor = System.Drawing.Color.Peru;
+            this.BtnPlatos.BackgroundColor = System.Drawing.Color.Peru;
+            this.BtnPlatos.BorderColor = System.Drawing.Color.SeaGreen;
+            this.BtnPlatos.BorderRadius = 14;
+            this.BtnPlatos.BorderSize = 0;
+            this.BtnPlatos.FlatAppearance.BorderSize = 0;
+            this.BtnPlatos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnPlatos.Font = new System.Drawing.Font("Source Sans Pro", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnPlatos.ForeColor = System.Drawing.Color.White;
+            this.BtnPlatos.Location = new System.Drawing.Point(3, 29);
+            this.BtnPlatos.Name = "BtnPlatos";
+            this.BtnPlatos.Size = new System.Drawing.Size(274, 84);
+            this.BtnPlatos.TabIndex = 4;
+            this.BtnPlatos.Text = "Insertar imágenes de platos";
+            this.BtnPlatos.TextColor = System.Drawing.Color.White;
+            this.BtnPlatos.UseVisualStyleBackColor = false;
+            this.BtnPlatos.Click += new System.EventHandler(this.BtnPlatos_Click);
+            // 
+            // BtnCategorias
+            // 
+            this.BtnCategorias.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnCategorias.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.BtnCategorias.BackgroundColor = System.Drawing.Color.PaleVioletRed;
+            this.BtnCategorias.BorderColor = System.Drawing.Color.SeaGreen;
+            this.BtnCategorias.BorderRadius = 11;
+            this.BtnCategorias.BorderSize = 0;
+            this.BtnCategorias.FlatAppearance.BorderSize = 0;
+            this.BtnCategorias.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnCategorias.Font = new System.Drawing.Font("Source Sans Pro", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCategorias.ForeColor = System.Drawing.Color.White;
+            this.BtnCategorias.Location = new System.Drawing.Point(303, 29);
+            this.BtnCategorias.Name = "BtnCategorias";
+            this.BtnCategorias.Size = new System.Drawing.Size(274, 84);
+            this.BtnCategorias.TabIndex = 3;
+            this.BtnCategorias.Text = "Insertar imágenes de categorías";
+            this.BtnCategorias.TextColor = System.Drawing.Color.White;
+            this.BtnCategorias.UseVisualStyleBackColor = false;
+            this.BtnCategorias.Click += new System.EventHandler(this.BtnCategorias_Click);
+            // 
+            // BtnUsuarios
+            // 
+            this.BtnUsuarios.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnUsuarios.BackColor = System.Drawing.Color.SlateBlue;
+            this.BtnUsuarios.BackgroundColor = System.Drawing.Color.SlateBlue;
+            this.BtnUsuarios.BorderColor = System.Drawing.Color.SeaGreen;
+            this.BtnUsuarios.BorderRadius = 14;
+            this.BtnUsuarios.BorderSize = 0;
+            this.BtnUsuarios.FlatAppearance.BorderSize = 0;
+            this.BtnUsuarios.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnUsuarios.Font = new System.Drawing.Font("Source Sans Pro", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnUsuarios.ForeColor = System.Drawing.Color.White;
+            this.BtnUsuarios.Location = new System.Drawing.Point(603, 29);
+            this.BtnUsuarios.Name = "BtnUsuarios";
+            this.BtnUsuarios.Size = new System.Drawing.Size(275, 84);
+            this.BtnUsuarios.TabIndex = 5;
+            this.BtnUsuarios.Text = "Insertar imágenes de usuarios";
+            this.BtnUsuarios.TextColor = System.Drawing.Color.White;
+            this.BtnUsuarios.UseVisualStyleBackColor = false;
+            this.BtnUsuarios.Click += new System.EventHandler(this.BtnUsuarios_Click);
+            // 
+            // BtnTodo
+            // 
+            this.BtnTodo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.BtnTodo.BackColor = System.Drawing.Color.SeaGreen;
+            this.BtnTodo.BackgroundColor = System.Drawing.Color.SeaGreen;
+            this.BtnTodo.BorderColor = System.Drawing.Color.SeaGreen;
+            this.BtnTodo.BorderRadius = 14;
+            this.BtnTodo.BorderSize = 0;
+            this.BtnTodo.FlatAppearance.BorderSize = 0;
+            this.BtnTodo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnTodo.Font = new System.Drawing.Font("Source Sans Pro", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnTodo.ForeColor = System.Drawing.Color.White;
+            this.BtnTodo.Location = new System.Drawing.Point(324, 568);
+            this.BtnTodo.Name = "BtnTodo";
+            this.BtnTodo.Size = new System.Drawing.Size(378, 82);
+            this.BtnTodo.TabIndex = 2;
+            this.BtnTodo.Text = "Insertar todas las imágenes";
+            this.BtnTodo.TextColor = System.Drawing.Color.White;
+            this.BtnTodo.UseVisualStyleBackColor = false;
+            this.BtnTodo.Click += new System.EventHandler(this.BtnTodo_Click);
+            // 
+            // FrmImageUpload
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1027, 703);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmImageUpload";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Garmoxu Image Upload";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private RJCodeAdvance.RJControls.RJRadioButton RdbLocal;
+        private RJCodeAdvance.RJControls.RJRadioButton RdbRemoto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private RJCodeAdvance.RJControls.RJButton BtnPlatos;
+        private RJCodeAdvance.RJControls.RJButton BtnCategorias;
+        private RJCodeAdvance.RJControls.RJButton BtnUsuarios;
+        private RJCodeAdvance.RJControls.RJButton BtnTodo;
+    }
+}
+
---
+++ Garmoxu_ImageUpload/Garmoxu_ImageUpload/FrmImageUpload.cs
@@ -0,0 +1,177 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Garmoxu_ImageUpload
+{
+    public partial class FrmImageUpload : Form
+    {
+        // Instancia de la conexión a la base de datos.
+        private MySqlConnection ConexionBD;
+
+        public FrmImageUpload()
+        {
+            InitializeComponent();
+            RdbLocal.Checked = true;
+        }
+
+        #region Cambio de conexión
+        private void RdbLocal_CheckedChanged(object sender, EventArgs e)
+        {
+            if (RdbLocal.Checked)
+            {
+                RdbRemoto.Checked = false;
+                CambiarModoConexionBD();
+            }
+        }
+
+        private void RdbRemoto_CheckedChanged(object sender, EventArgs e)
+        {
+            if (RdbRemoto.Checked)
+            {
+                RdbLocal.Checked = false;
+                CambiarModoConexionBD();
+            }
+        }
+
+        // Abre la conexion a la base de datos.
+        private void CambiarModoConexionBD()
+        {
+            // Conexión local
+            string servidorLocal = "localhost"; //Nombre o IP del servidor.
+            string bdLocal = "garmoxu"; //Nombre de la base de datos.
+            string usuarioLocal = "root"; //Usuario de acceso.
+            string passwordLocal = "root"; //Contraseña de usuario de acceso.
+
+            // Conexión remota
+            string servidorRemoto = "sql781.main-hosting.eu"; //Nombre o IP del servidor.
+            string bdRemoto = "u184120704_garmoxudb"; //Nombre de la base de datos.
+            string usuarioRemoto = "u184120704_admindam"; //Usuario de acceso.
+            string passwordRemoto = "damAdmin123"; //Contraseña de usuario de acceso.
+
+            if (RdbLocal.Checked)
+            {
+                ConexionBD = new MySqlConnection(
+                    "Database=" + bdLocal + "; Data Source=" + servidorLocal + "; User Id=" + usuarioLocal + "; Password=" + passwordLocal + ";");
+            }
+            else
+            {
+                ConexionBD = new MySqlConnection(
+                    "Database=" + bdRemoto + "; Data Source=" + servidorRemoto + "; User Id=" + usuarioRemoto + "; Password=" + passwordRemoto + ";");
+            }
+        }
+        #endregion Cambio de conexión
+
+        #region Platos
+        private void BtnPlatos_Click(object sender, EventArgs e)
+        {
+            InsertarImagenesPlatos();
+            MessageBox.Show("¡Operación completada con éxito!", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        #endregion Platos
+
+        #region Categorias
+        private void BtnCategorias_Click(object sender, EventArgs e)
+        {
+            InsertarImagenesCategorias();
+            MessageBox.Show("¡Operación completada con éxito!", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        #endregion Categorias
+
+        #region Usuarios
+        private void BtnUsuarios_Click(object sender, EventArgs e)
+        {
+            InsertarImagenesUsuarios();
+            MessageBox.Show("¡Operación completada con éxito!", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        #endregion Usuarios
+
+        #region Todas las imágenes
+        private void BtnTodo_Click(object sender, EventArgs e)
+        {
+            InsertarImagenesPlatos();
+            InsertarImagenesCategorias();
+            InsertarImagenesUsuarios();
+            MessageBox.Show("¡Operación completada con éxito!", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        #endregion Todas las imágenes
+
+        #region Inserción de imagenes
+        private void InsertarImagenesPlatos()
+        {
+            InsertarImagenes("PlatosComida", "ImagenPlato", "IdPlatoComida");
+        }
+
+        private void InsertarImagenesCategorias()
+        {
+            InsertarImagenes("Categorias", "ImagenCategoria", "IdCategoria");
+        }
+
+        private void InsertarImagenesUsuarios()
+        {
+            InsertarImagenes("Usuarios", "ImagenUsuario", "NombreUsuario");
+        }
+        private void InsertarImagenes(string tabla, string columnaDeImagen, string columnaId)
+        {
+            try
+            {
+                ConexionBD.Open();
+
+                List<string> claves = new List<string>();
+                List<byte[]> imagenBytes = new List<byte[]>();
+                RecogerClavesEImagenes(ref claves, ref imagenBytes, tabla);
+
+                for (int i = 0; i < claves.Count; i++)
+                {
+                    string sql = string.Format("UPDATE {0} SET {1} = @imagen WHERE {2} = '{3}'", tabla, columnaDeImagen, columnaId, claves[i]);
+                    MySqlCommand cmd = new MySqlCommand(sql, ConexionBD);
+                    cmd.Parameters.Add("@imagen", MySqlDbType.MediumBlob).Value = imagenBytes[i];
+                    cmd.ExecuteNonQuery();
+                }
+
+            }
+            catch (AggregateException ex)
+            {
+                string mensaje = "No se ha podido establecer la conexión al servidor, revise su conexión a internet y el estado del servidor.";
+                if (MessageBox.Show(mensaje, "", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error).Equals(DialogResult.Retry)) Application.Restart();
+                else Environment.Exit(0);
+            }
+            catch (Exception ex)
+            {
+                string mensaje = "Ha ocurrido el siguiente error de tipo '" + ex.GetType().Name + "' : \n" + ex.Message;
+                MessageBox.Show(mensaje, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            if(ConexionBD.State.Equals(ConnectionState.Open)) ConexionBD.Close();
+        }
+
+        private void RecogerClavesEImagenes(ref List<string> claves, ref List<byte[]> imagenBytes, string rutaDirectorio)
+        {
+            DirectoryInfo dir = new DirectoryInfo(rutaDirectorio);
+            if (dir.Exists)
+            {
+                FileInfo[] imagenes = dir.GetFiles();
+                if (imagenes.Length != 0)
+                {
+                    StreamReader reader = new StreamReader(imagenes[0].FullName);
+                    foreach (FileInfo imagen in imagenes)
+                    {
+                        reader = new StreamReader(imagen.FullName);
+                        claves.Add(Path.GetFileNameWithoutExtension(imagen.Name));
+                        imagenBytes.Add(File.ReadAllBytes(imagen.FullName));
+                    }
+                    reader.Close();
+                }
+            }
+        }
+        #endregion Inserción de imagenes
+    }
+}
